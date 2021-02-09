9a7c4ddc27b96b17c94d66947850476a9335ce56
Volkan <volky@github.com>
2021-02-09 10:23:17 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        Musteri musteri = new Musteri();
+
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri bilgileri başarıyla eklendi!!!");
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Adı : " + musteri.Ad);              
+                Console.WriteLine("Yaşı : " + musteri.yas);
+                Console.WriteLine();
+            }
+
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " Numaralı müşterinin bilgileri başarıyla silindi !!!");
+        }
+    }
+}
+
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Volkan";
+            musteri1.yas = 22;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Olcay";
+            musteri2.Id = 8;
+
+
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2};
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            Console.WriteLine(musteri1);
+            Console.WriteLine(musteri2);
+
+            Console.ReadLine();
+            musteriManager.MusteriListele(musteriler);
+
+            Console.WriteLine(musteriler);
+            
+
+            Console.ReadLine();
+
+            musteriManager.MusteriSil(musteri1);
+            musteriManager.MusteriSil(musteri2);
+
+            Console.WriteLine(musteri1);
+            Console.WriteLine(musteri2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
